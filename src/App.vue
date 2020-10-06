7dea485669660a21b9840b48ed0e099a4b59076e
fumeboy <fumeboy@qq.com>
2020-10-07 00:29:10 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,261 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <v-app id="inspire">
+        <v-navigation-drawer
+                app
+        >
+            <v-text-field
+                    flat
+                    solo
+                    filled
+                    hide-details
+                    prepend-inner-icon="mdi-pen"
+                    :append-icon="'mdi-plus'"
+                    @click:append="addColor"
+                    background-color="#f1f1f1"
+                    label="color"
+                    v-model="color_selector"
+            ></v-text-field>
+            <div class="color-viewer" :style="'background-color: '+color_selector"></div>
+            <div>
+              <div v-for="item in history" :key="item.i" class="color-item">
+                <v-btn class="color-box" small depressed :style="'background-color: '+item.v"></v-btn>
+                <v-btn text small @click="()=>setBgColor(item.v)">Background</v-btn>
+                <v-btn text small @click="()=>setColor(item.v)">Pen</v-btn>
+              </div>
+            </div>
+        </v-navigation-drawer>
+        <v-main style="background-color: rgba(241, 241, 241, 0.5)">
+          <v-container
+                  class="fill-height"
+                  fluid
+          >
+            <v-row
+                    align="center"
+                    justify="center"
+            >
+              <canvas id="canvas" ref="canvas"></canvas>
+            </v-row>
+            <div class="now-color">
+              <div class="pen">
+                <div class="color-box" :style="'background-color: '+penColor"></div>
+                <div class="bar">
+                  <p> <b>画笔颜色</b> </p>
+                  <p>{{penColor}}</p>
+                </div>
+              </div>
+              <div class="bg">
+                <div class="color-box" :style="'background-color: '+bgColor"></div>
+                <div class="bar">
+                  <p> <b> 背板颜色</b> </p>
+                  <p>{{bgColor}}</p>
+                </div>
+              </div>
+            </div>
+          </v-container>
+        </v-main>
+        <v-speed-dial
+                bottom
+                right
+                fixed
+                v-model="fab"
+        >
+          <template v-slot:activator>
+          <v-btn
+                  v-model="fab"
+                  color="pink"
+                  dark
+                  depressed
+                  fab
+          >
+            <v-icon v-if="fab">mdi-close</v-icon>
+            <v-icon v-else>mdi-plus</v-icon>
+          </v-btn>
+          </template>
+          <v-btn
+                  color="pink"
+                  dark
+                  fab
+                  depressed
+                  @click="dialog = !dialog"
+          >
+            <v-icon>mdi-export</v-icon>
+          </v-btn>
+          <v-btn
+                  color="pink"
+                  dark
+                  fab
+                  depressed
+                  @click="dialog2 = true"
+          >
+            <v-icon>mdi-help</v-icon>
+          </v-btn>
+        </v-speed-dial>
+        <v-dialog
+                v-model="dialog"
+                width="800px"
+        >
+          <v-card>
+            <v-card-title>
+               导出图片
+            </v-card-title>
+            <v-container>
+              <v-row class="mx-2">
+                <v-col
+                        class="align-center justify-space-between"
+                        cols="12"
+                >
+                    <v-text-field
+                            label="文件名"
+                            v-model="export_filename"
+                    ></v-text-field>
+                </v-col>
+<!--                <v-col cols="6">-->
+<!--                  <v-text-field-->
+<!--                          label="导出大小 (16 x 16 的倍数 )"-->
+<!--                          v-model="export_size"-->
+<!--                  ></v-text-field>-->
+<!--                </v-col>-->
+              </v-row>
+            </v-container>
+            <v-card-actions>
+              <v-btn
+                      text
+                      color="primary"
+              >More</v-btn>
+              <v-spacer></v-spacer>
+              <v-btn
+                      text
+                      color="primary"
+                      @click="dialog = false"
+              >Cancel</v-btn>
+              <v-btn
+                      text
+                      @click="()=>{dialog = false;exportt()}"
+              >Save</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog
+                width="800px"
+                v-model="dialog2"
+        >
+          <v-card>
+            <v-card-title>
+              说明
+            </v-card-title>
+            <v-container>
+              <v-row class="mx-2">
+                <v-col
+                        class="align-center justify-space-between"
+                        cols="12"
+                >
+                  本网页程序应当在PC端使用。
+                  左侧是调色器以及色彩板，可以选择笔刷颜色和背景颜色
+                  （导出图片时，背景颜色不显示）
+                </v-col>
+              </v-row>
+            </v-container>
+            <v-card-actions>
+              <v-btn text @click="dialog2=false">OK</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {Run} from './draw.js'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    fab: false,
+    export_filename:"a1",
+    cvs: null,
+    penColor: "#000",
+    bgColor: "#fff",
+    color_selector: "#000",
+    dialog: false,
+    dialog2: false,
+    history: [
+      {v:'#000000',i:0},
+      {v:'#123456',i:1}
+    ],
+  }),
+  methods:{
+    setColor(color){
+      this.cvs.SetColor(color)
+      this.penColor = color
+    },
+    setBgColor(color){
+      this.bgColor = color
+      this.cvs.bgColor = color
+      this.cvs.drawBg()
+    },
+    addColor(){
+      this.history.push({v:this.color_selector, i:this.history.length})
+    },
+    exportt(){
+      this.cvs.export(this.export_filename)
+    },
+  },
+  mounted() {
+    let canvas = this.$refs.canvas;
+    this.cvs = new Run(canvas);
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  #canvas{
+    border: 1px solid #ddd;
+  }
+  .inputt {
+    border-radius: 0;
+    background: #eee;
+  }
+  .color-viewer{
+    height: 2rem;
+    width: 100%;
+  }
+  .color-item{
+    display: flex;
+    margin: 1rem 0 0 1rem;
+    padding: 0.1rem;
+    border: 1px solid #f1f1f1;
+  }
+  .color-item button{
+    /*width: 30%;*/
+    color: #000
+  }
+  .color-item .color-box{
+    /*height: 2rem;*/
+    width: 20%;
+  }
+   .now-color{
+      position: absolute;
+     left: 1rem;
+     bottom: 1rem;
+   }
+   .now-color > div{
+     display: grid;
+     grid-template: 1fr/1fr 2fr;
+     margin: 1rem 0;
+   }
+   .now-color .bar{
+     display: flex;
+     font-size: .9rem;
+     height: 100%;
+     justify-content: center;
+     flex-direction: column;
+   }
+  .now-color .bar p{
+    margin: 0;
+  }
+    .now-color .color-box{
+      height: 3rem;
+      width: 3rem;
+      margin: 0 1rem;
+      border: 1px solid #ddd;
+    }
 </style>
---
+++ src/draw.js
@@ -0,0 +1,254 @@
+function Run(canvas) {
+    this.bgColor = "#fff"
+    this.canvas = canvas;
+    this.cvs = canvas.getContext("2d");
+    this.penColor = "#CD6155";//画笔颜色
+    this.ifClear = false; // 是否是橡皮擦状态 
+    this.boxSize = null;//单个格子大小
+    this.colorArr = new Set();//记录使用过的颜色
+    this.size = 16;//格子数（边）
+    this.gridArr = new Array(this.size*this.size);//画布所有格子，标记有无绘画
+    this.distanceX = null;//画布在屏幕中相对位置
+    this.distanceY = null;
+    this.preX = null;//上一次绘画坐标
+    this.preY = null;
+    this.start();
+    this.click();
+    this.touchstart();
+    return this
+}
+Run.prototype.SetColor = function(color){
+    if (color){
+        this.penColor = color
+        this.ifClear = false
+    }else{
+        this.penColor = this.bgColor
+        this.ifClear = true
+    }
+}
+
+//计算canvas在屏幕中的位置，进而计算触摸点与canvas的相对位置
+Run.prototype.computerWindow = function () {
+    this.distanceX = this.canvas.getBoundingClientRect().x;
+    this.distanceY = this.canvas.getBoundingClientRect().y;
+};
+//初始化
+Run.prototype.start = function () {
+    let cw;
+    if (window.innerHeight > window.innerWidth) {
+        cw = parseInt(window.innerWidth * 0.85);
+    } else {
+        cw = parseInt(window.innerHeight * 0.85);
+    }
+    cw = parseInt(cw / this.size) * this.size;
+    this.canvas.width = this.canvas.height = cw;
+    this.boxSize = cw / this.size;
+    this.computerWindow();
+    this.drawBg();
+};
+//当触摸按下时
+Run.prototype.touchstart = function () {
+    let move = this.touchmove.bind(this);
+    this.canvas.addEventListener("touchstart", function (e) {
+        e.preventDefault();
+        let dot = e.changedTouches[0];
+        let o = this.computedXY(dot.pageX, dot.pageY, true);
+        this.beforeDraw(o.re_x, o.re_y);
+        this.canvas.addEventListener("touchmove", throttle(move, 5, 20))
+    }.bind(this));
+    this.canvas.addEventListener("touchend", function () {
+        this.canvas.removeEventListener("touchmove", move)
+    }.bind(this))
+};
+//当鼠标按下时
+Run.prototype.click = function () {
+    let move = this.mousemove.bind(this);
+    this.canvas.addEventListener("mousedown", function (e) {
+        e.preventDefault();
+        let o = this.computedXY(e.offsetX, e.offsetY);
+        this.beforeDraw(o.re_x, o.re_y);
+        this.canvas.addEventListener("mousemove", move);
+    }.bind(this));
+    this.canvas.addEventListener("mouseleave", function () {
+        this.canvas.removeEventListener("mousemove", move);
+    }.bind(this))
+    this.canvas.addEventListener("mouseup", function () {
+        this.canvas.removeEventListener("mousemove", move);
+    }.bind(this))
+};
+//触摸移动时
+Run.prototype.mousemove = function (e) {
+    let o = this.computedXY(e.offsetX, e.offsetY);
+    if((!o.re_y)||(!o.re_x))return ;
+    this.beforeDraw(o.re_x, o.re_y, true)
+};
+//鼠标移动时
+Run.prototype.touchmove = function (e) {
+    let dot = e.changedTouches[0];
+    let o = this.computedXY(dot.pageX, dot.pageY, true);
+    if((!o.re_y)||(!o.re_x))return ;
+    if (!(o.re_x === this.preX && o.re_y === this.preY)) this.beforeDraw(o.re_x, o.re_y, true);
+};
+//根据坐标计算出是在那个格子
+Run.prototype.computedXY = function (x, y, z) {
+    if (z) {
+        x = x - this.distanceX;
+        y = y - this.distanceY;
+    }
+    let temp_x = x;
+    let temp_y = y;
+    let re_x, re_y;
+    for (let i = 0; i < this.size; i++) {
+        temp_x = temp_x - this.boxSize;
+        if (temp_x < 0) {
+            re_x = i;
+            break;
+        }
+    }
+    for (let i = 0; i < this.size; i++) {
+        temp_y = temp_y - this.boxSize;
+        if (temp_y < 0) {
+            re_y = i;
+            break;
+        }
+    }
+    return {re_x, re_y}
+};
+//开始画格子之前
+Run.prototype.beforeDraw = function (x, y, z) {
+    // z 表示处于滑动状态
+    // 如果移动得太快，线条会断开
+    // 这里的if 多少完善了断线的问题
+    if (z && (Math.abs(y - this.preY) + Math.abs(x - this.preX)) > 2) {
+        let dx, dy, lenx, leny, tempx, tempy;
+        leny = Math.abs(y - this.preY);
+        lenx = Math.abs(x - this.preX);
+        tempx = this.preX;
+        tempy = this.preY;
+        if (y > this.preY) {
+            dy = 1
+        } else {
+            dy = -1
+        }
+        if (x > this.preX) {
+            dx = 1
+        } else {
+            dx = -1
+        }
+        if (leny > lenx) {
+            for (let i = 0; i < leny; i++) {
+                tempy = tempy + dy;
+                if (lenx > 0) {
+                    tempx = tempx + dx;
+                    this.drawUnit(tempx , tempy);
+                    this.gridArr[tempx + this.size * tempy] = this.ifClear?0:1;
+                    lenx--;
+                } else {
+                    this.drawUnit(tempx , tempy);
+                    this.gridArr[tempx + this.size * tempy] = this.ifClear?0:1;
+                }
+            }
+        } else {
+            for (let i = 0; i < lenx; i++) {
+                tempx = tempx + dx;
+                if (leny > 0) {
+                    tempy = tempy + dy;
+                    this.drawUnit(tempx , tempy);
+                    this.gridArr[tempx + this.size * tempy] = this.ifClear?0:1;
+                    leny--;
+                } else {
+                    this.drawUnit(tempx , tempy);
+                    this.gridArr[tempx + this.size * tempy] = this.ifClear?0:1;
+                }
+            }
+        }
+    } else {
+        this.drawUnit(x, y);
+        this.gridArr[x + this.size * y] = this.ifClear?0:1;
+    }
+    this.preX = x;
+    this.preY = y;
+    this.colorArr.add(this.penColor);
+};
+Run.prototype.clean = function () {
+    this.gridArr = new Array(this.size*this.size);
+    this.drawBg();
+};
+
+Run.prototype.drawBg = function () {
+    let temp_color = this.penColor;
+    this.penColor = this.bgColor;
+    for (let i = 0; i < this.size; i++) {
+        for (let j = 0; j < this.size; j++) {
+            if(!this.gridArr[i + this.size * j])this.drawUnit(i, j)
+        }
+    }
+    //再切换为画笔颜色
+    this.penColor = temp_color;
+};
+
+Run.prototype.clearBg = function () {
+    for (let i = 0; i < this.size; i++) {
+        for (let j = 0; j < this.size; j++) {
+            if(!this.gridArr[i + this.size * j])this.cleanUnit(i, j)
+        }
+    }
+};
+
+Run.prototype.drawUnit = function (x, y) {
+    let pos_x = x * this.boxSize
+    let pos_y = y * this.boxSize
+    this.cvs.beginPath();
+    this.cvs.fillStyle = this.penColor;
+    this.cvs.fillRect(pos_x, pos_y, this.boxSize, this.boxSize);
+    this.cvs.fill();
+    this.cvs.stroke();
+    this.cvs.closePath()
+};
+
+Run.prototype.cleanUnit = function (x, y) {
+    let pos_x = x * this.boxSize
+    let pos_y = y * this.boxSize
+    this.cvs.clearRect(pos_x, pos_y, this.boxSize, this.boxSize);
+};
+
+Run.prototype.export = function(filename){
+    var MIME_TYPE = "image/png";
+    this.clearBg()
+    var imgURL = this.canvas.toDataURL(MIME_TYPE);
+
+    var dlLink = document.createElement('a');
+    dlLink.download = filename;
+    dlLink.href = imgURL;
+    dlLink.dataset.downloadurl = [MIME_TYPE, dlLink.download, dlLink.href].join(':');
+    this.drawBg()
+    document.body.appendChild(dlLink);
+    dlLink.click();
+    document.body.removeChild(dlLink);
+
+}
+
+//一个优化函数，touchmove 和 mousemove 会在移动时很频繁地发生
+//用这个函数，让它最多 10（n）ms 进行一次，n 在调用处声明
+let throttle = function (fn, delay, mustRunDelay) {
+    let timer = null;
+    let t_start;
+    return function () {
+        let context = this, args = arguments, t_curr = +new Date();
+        clearTimeout(timer);
+        if (!t_start) {
+            t_start = t_curr;
+        }
+        if (t_curr - t_start >= mustRunDelay) {
+            fn.apply(context, args);
+            t_start = t_curr;
+        }
+        else {
+            timer = setTimeout(function () {
+                fn.apply(context, args);
+            }, delay);
+        }
+    };
+};
+
+export {Run}
